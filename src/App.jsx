4b7160e954b4c17d4aa5b14e5b1ffb55ffb8fccd
Ricardo <rick-queiroz@live.com>
2024-07-19 18:27:18 -0300

    feat:create Coin  and routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from './components/NavBar/NavBar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import Coin from './pages/Coin/Coin'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <NavBar/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/coin/:coinId' element={<Coin/>}></Route>
       </Routes>
     </div>
   )
