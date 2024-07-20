2cc6f951eabb66cddfea0624255bb5fd71e5fa1e
Ricardo <rick-queiroz@live.com>
2024-07-19 21:53:47 -0300

    feat:(Home)add content to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 import './Home.css'
 const Home = () => {
   return (
-    <div>
-      
+    <div className='home'>
+      <div className="hero">
+        <h1>Largest <br /> Crypto Marketplace</h1>
+        <p>Welcome to crypto marketplace.</p>
+        <form>
+          <input type="text" placeholder='Search crypto...'/>
+          <button>Search</button>
+        </form>
+      </div>
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p style={{textAlign:"center"}}>24H Change</p>
+          <p style={{textAlign:"end"}}>Market Cap</p>
+        </div>
+      </div>
     </div>
   )
 }
