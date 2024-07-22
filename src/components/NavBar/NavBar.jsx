1a6201d209b95fefeee01866bc235906ab305cb9
Ricardo <rick-queiroz@live.com>
2024-07-21 22:02:21 -0300

    creates currency exchange functionality

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './NavBar.css'
+import { CoinContext } from '../../context/CoinContext'
 
 const NavBar = () => {
+  
+  const {setCurrency} = useContext(CoinContext)
+
+  const currencyHandler = (event) => {
+    switch (event.target.value){
+      case "usd" : {
+        setCurrency({name : "usd", symbol: "$"})
+        break
+      }
+      case "eur" : {
+        setCurrency({name : "eur", symbol: "€"})
+        break
+      }
+      case "inr" : {
+        setCurrency({name : "inr", symbol: "₹"})
+        break
+      }
+      default : {
+        setCurrency({name : "usd", symbol: "$"})
+        break
+      }
+    }
+  }
+
   return (
     <div className='navbar'>
       <h1>CryptoHub</h1>
@@ -12,7 +37,7 @@ const NavBar = () => {
         <li>Blog</li>
       </ul>
       <div className="nav-right">
-        <select>
+        <select onChange={currencyHandler}>
           <option value="usd ">USD</option>
           <option value="eur">EUR</option>
           <option value="inr">INR</option>
